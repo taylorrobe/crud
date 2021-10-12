3f4b4cc3c678fe7b5a11d71b0933dbea68d6b911
Rob Taylor <robtaylor@adviserplus.com>
2021-10-12 09:49:29 +0100

    initial

--- Crud-u-like/Crud-u-like/Controllers/ConveyanceCaseController.cs
+++ Crud-u-like/Crud-u-like/Controllers/ConveyanceCaseController.cs
@@ -1,8 +1,4 @@
-﻿using Crud_u_like.Entities;
-using Crud_u_like.Services;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using Crud_u_like.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Crud_u_like.Controllers
 {
@@ -10,45 +6,6 @@ namespace Crud_u_like.Controllers
     [ApiController]
     public class ConveyanceCaseController : ControllerBase
     {
-        private readonly IConveyanceCaseService _conveyanceCaseService;
 
-        public ConveyanceCaseController(IConveyanceCaseService conveyanceCaseService)
-        {
-            _conveyanceCaseService = conveyanceCaseService;
-        }
-
-        [HttpGet]
-        [Route("/[action]")]
-        public IEnumerable<ConveyanceCase> GetAll()
-        {
-            return _conveyanceCaseService.GetAll();
-        }
-
-        [HttpGet("/[action]{id}")]
-        public ConveyanceCase Get(int id)
-        {
-            return _conveyanceCaseService.Get(id);
-        }
-
-        [HttpPost]
-        [Route("/[action]")]
-        public void Post([FromBody] CreateConveyanceCaseModel conveyanceCaseModel)
-        {
-            _conveyanceCaseService.Post(conveyanceCaseModel);
-        }
-
-        [HttpPut]
-        [Route("/[action]")]
-        public void Put([FromBody] ConveyanceCase conveyanceCase)
-        {
-            _conveyanceCaseService.Put(conveyanceCase);
-        }
-
-        [HttpDelete]
-        [Route("/[action]")]
-        public void Delete(int id)
-        {
-            _conveyanceCaseService.Delete(id);
-        }
     }
 }
--- Crud-u-like/Crud-u-like/Tests/ConveyanceCaseTests.cs
+++ Crud-u-like/Crud-u-like/Tests/ConveyanceCaseTests.cs
@@ -1,9 +1,5 @@
 ﻿using NUnit.Framework;
-using Crud_u_like.Tests.Creators.Service;
-using Crud_u_like.Models;
 using Testify;
-using Crud_u_like.Enums;
-using Crud_u_like.Tests.Creators.Repository;
 
 namespace Crud_u_like.Tests
 {
@@ -12,27 +8,5 @@ namespace Crud_u_like.Tests
     {
         private static readonly AnonymousData AnonymousData = new AnonymousData();
 
-        [Test]
-        public void CreateConveyanceCaseTest()
-        {
-            var service = ConveyanceCaseServiceCreator.CreateService();
-
-            var model = CreateConveyanceCaseModel.Create(
-                (int)AnonymousData.AnyEnumValue<CaseTypeEnum>(), 
-                (int)AnonymousData.AnyEnumValue<CaseStatusEnum>(), 
-                AnonymousData.AnyString(), 
-                AnonymousData.AnyDecimal(), 
-                false);
-
-            var caseId = service.Post(model);
-
-            var dbCase = ConveyanceCaseRepositoryCreator.GetConveyanceCase(caseId);
-            Assert.IsNotNull(dbCase);
-            Assert.AreEqual(caseId, dbCase.Id);
-            Assert.AreEqual(model.CaseTypeId, dbCase.CaseTypeId);
-            Assert.AreEqual(model.CaseStatusId, dbCase.CaseStatusId);
-            Assert.AreEqual(model.TransactionAmount, dbCase.TransactionAmount);
-            Assert.AreEqual(model.AddressLine, dbCase.Address.AddressLine);
-        }
     }
 }
